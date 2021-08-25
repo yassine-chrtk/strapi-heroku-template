f445a5bed0d153c7d4c037b19b84b943013763e9
Yassine C <7108470-yassine_chrtk@users.noreply.gitlab.com>
2021-08-25 16:33:01 +0000

    Mettre à jour config/env/staging/.gitkeep, config/env/staging/database.js, config/env/staging/server.js, config/env/production/database.js files

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
+// Use this configuration for an SQLite database on your machine.
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
-        ssl: true,
+        useNullAsDefault: true,
       },
     },
   },
